3fd3e9f786d5c3fb82da12dde4315f9764cba6af
decoder <decoder@lilve.de>
2021-02-04 21:05:35 +0100

    exercise 4

---
+++ 001-variables-values-types/4-exercise.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type age int
+
+var x age
+
+// Printf formats string, Println just prints value
+func main(){
+	fmt.Println(x)
+	fmt.Printf("%T\t", x)
+
+	x = 42
+	fmt.Println(x)
+}
