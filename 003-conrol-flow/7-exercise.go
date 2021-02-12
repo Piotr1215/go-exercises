856b10ea34d3e5569d2824aed6c25a2217069d6d
decoder <decoder@lilve.de>
2021-02-12 22:58:02 +0100

    control flow

---
+++ 003-conrol-flow/7-exercise.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+)
+
+func main() {
+
+	var prgName, firstLetter string
+
+	if os.Args[0] != "" {
+		prgName = os.Args[1:]
+	} else {
+		prgName = "Decoder"
+	}
+
+	firstLetter = string(prgName[0])
+
+	if firstLetter == strings.ToUpper(firstLetter) {
+		fmt.Println("Name of", prgName, "is correctly capitalized")
+	} else {
+		fmt.Println("Name of", prgName, "is not correctly capitalized")
+	}
+}
---
+++ 003-conrol-flow/9-exercise.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+)
+
+func main()  {
+	var favSport string = "Judo"
+
+	switch favSport {
+	case "Karate":
+		fmt.Println("Favourite sport is Karate")
+	case "Judo":
+		fmt.Println("Favourite sport is judo")
+	default:
+		fmt.Println("No favourite sport")
+	}
+}
